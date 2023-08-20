a716005d4dcf51b41f127f4f891d3cfa148cff9c
unknown <malikhax3@gmail.com>
2023-08-20 12:43:05 +0500

    formated price

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,12 +1,14 @@
+import { AddCardTypes } from "@/types/AddCardTypes"
+import formatPrice from "@/util/PriceFormat"
 import Image from "next/image"
 
-const Product=({name,image,price})=> {
+const Product=({name,image,price}:AddCardTypes)=> {
   return (
     <div>
         <Image src={image} alt={name} width={400} height={400} />
         <h1>{name}</h1>
-        {price}
-    </div>
+        {formatPrice(price as number )}
+    </div> 
   )
 }
 
---
+++ src/types/AddCardTypes.ts
@@ -0,0 +1,5 @@
+export type AddCardTypes ={
+    name:string
+    image:string
+    price: number | null
+}
